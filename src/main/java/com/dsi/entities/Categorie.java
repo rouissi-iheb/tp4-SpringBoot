e5aa89f277c7ad575745be80caf519c23c4d4557
Rouissi Iheb <61634891+rouissi-iheb@users.noreply.github.com>
2020-11-11 21:39:14 +0000

    Add files via upload
    Spring boot Licence Key Manager project .
    Live test : http://rouissi-iheb-dsi31-springboot.tk

---
+++ src/main/java/com/dsi/entities/Categorie.java
@@ -0,0 +1,31 @@
+package com.dsi.entities;
+
+import java.util.List;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Categorie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCat;
+	private String nomCat;
+	private String descriptionCat;
+	
+	@OneToMany(mappedBy = "categorie")
+	private List<AKey> keys;
+
+}
